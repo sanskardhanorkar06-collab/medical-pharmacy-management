660c294c025bca34966e98051626a5db38fb8574
Prabhansh <sanskardhanorkar06@gmail.com>
2026-08-26 20:41:54 +0530

    Add database

--- App.py
+++ App.py
@@ -1,13 +1,39 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="YOUR_MYSQL_PASSWORD",
+        database="pharmacy_db"
+    )
+
+    return connection
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    connection = get_db_connection()
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM medicines")
+
+    medicines = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return render_template(
+        "index.html",
+        medicines=medicines
+    )
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
